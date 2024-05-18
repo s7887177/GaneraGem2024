c9ae983d89fc86179c990d8407801d6cfc70c35c
s7887177 <58907092+s7887177@users.noreply.github.com>
2024-05-18 17:30:47 +0800

    temp

--- Assets/Eason/Scripts/ItemViewManager.cs
+++ Assets/Eason/Scripts/ItemViewManager.cs
@@ -1,19 +1,44 @@
-﻿using Sirenix.OdinInspector;
+﻿using Oculus.Interaction;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ItemView Manager", menuName = "M Group/ItemView Manager")]
 public class ItemViewManager : MonoBehaviour
 {
-    [SerializeField, TableList] private ItemView[] _data;
-
-    public ItemView[] data { get => _data; set => _data = value; }
+    [SerializeField] private ItemManager _itemManager;
+    [SerializeField] private ItemView _itemPrefab;
+    [SerializeField, TableList] private ItemView[] _itemViews;
+    [SerializeField] private Transform _container;
+    public ItemView[] itemViews { get => _itemViews; set => _itemViews = value; }
 
     private void OnValidate()
     {
-        for (int i = 0; i < _data.Length; i++)
+        for (int i = 0; i < _itemViews.Length; i++)
         {
-            _data[i].id = i;
+            _itemViews[i].id = i;
         }
+    }
+
+    [Button("Create")]
+    private void EditorCreateItemViews()
+    {
+        var items = _itemManager.data;
+        var count = items.Length;
+        _itemViews = new ItemView[count];
+        EditorCleanItemViews();
+    }
+    [Button("Clean")]
+    private void EditorCleanItemViews()
+    {
+        if (_itemViews == null) return;
+        for (int i = 0; i < _itemViews.Length; i++)
+        {
+            Object.DestroyImmediate(_itemViews[i].gameObject);
+        }
+    }
+    private void EditorInitializeItem(Item item,ItemView itemView)
+    {
+        itemView.EditorInitialize(item);
     }
 }
 
--- Assets/M Group/Scripts/Controllers/ItemView.cs
+++ Assets/M Group/Scripts/Controllers/ItemView.cs
@@ -1,9 +1,23 @@
 using Sirenix.OdinInspector;
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ItemView : MonoBehaviour
 {
     [SerializeField, ReadOnly, TableColumnWidth(30)] private int _id;
-
+    [SerializeField] private Item _item;
+    [SerializeField] private MeshRenderer _renderer;
     public int id { get => _id; set => _id = value; }
+    public Item item { get => _item; }
+
+    internal void EditorInitialize(Item item)
+    {
+        _item = item;
+    }
+    private void OnEnable()
+    {
+        
+    }
+
 }
