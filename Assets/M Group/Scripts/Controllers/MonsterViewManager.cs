101200387867353d7f2e0bbe160f532b3f44853e
s7887177 <58907092+s7887177@users.noreply.github.com>
2024-05-19 14:35:40 +0800

    Add Monster View

---
+++ Assets/M Group/Scripts/Controllers/MonsterViewManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class MonsterViewManager : MonoBehaviour
+{
+    [SerializeField] private MonsterView[] _data;
+    [SerializeField] private IMonsterController _controller;
+
+
+    private void RegisterCollisionEvents()
+    {
+        for (int i = 0; i < _data.Length; i++)
+        {
+            var collider = _data[i].GetComponent<ColliderView>();
+            collider.onTouch += Collider_onTouch;
+        }
+    }
+
+    private void Collider_onTouch(ColliderView performer, ColliderView receiver)
+    {
+        switch (performer.Type)
+        {
+            case ColliderType.MonsterBody:
+                if(receiver.Type == ColliderType.Item)
+                {
+                    
+                }
+                break;
+            case ColliderType.MonsterWeakPoint:
+                break;
+            default:
+                break;
+        }
+
+        //_controller
+    }
+
+    private void UnegisterCollisionEvents()
+    {
+
+    }
+
+}
